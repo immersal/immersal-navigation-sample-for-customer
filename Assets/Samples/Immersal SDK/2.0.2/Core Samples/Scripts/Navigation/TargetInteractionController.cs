e623e3ab4e1a7425bb39c6133b9b461a1c56932e
cocoyuy <xy2141@nyu.edu>
2024-06-12 17:31:12 +0800

    add target user interaction

--- Assets/Samples/Immersal SDK/2.0.2/Core Samples/Scripts/Navigation/TargetInteractionController.cs
+++ Assets/Samples/Immersal SDK/2.0.2/Core Samples/Scripts/Navigation/TargetInteractionController.cs
@@ -9,6 +9,23 @@ namespace Immersal.Samples.Navigation
     {
         [SerializeField] private TextMeshPro popup = null;
         private bool isPopupVisible = false;
+        [SerializeField] private float popupDuration = 6.0f;
+        [SerializeField] private float heightOffset = 0.5f;
+
+        void Start()
+        {
+            if (popup != null)
+            {
+                Vector3 popupPosition = transform.position + new Vector3(0, heightOffset, 0);
+                popup.transform.position = popupPosition;
+                popup.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            }
+            else
+            {
+                Debug.LogWarning("Popup does not exist.");
+            }
+            popup.gameObject.SetActive(false);
+        }
 
         void Update()
         {
@@ -16,62 +33,28 @@ namespace Immersal.Samples.Navigation
             {
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hit;
-                 if (Physics.Raycast(ray, out hit))
+                if (Physics.Raycast(ray, out hit))
                 {
                     if (Vector3.Distance(hit.point, transform.position) <= 1.0f)
                     {
                         TogglePopup();
                     }
-                    else if (isPopupVisible)
-                    {
-                        HidePopup();
-                    }
-                }
-                else if (isPopupVisible)
-                {
-                    HidePopup();
                 }
             }
         }
 
         private void TogglePopup()
         {
-            if (!isPopupVisible)
-            {
-                ShowPopup();
-            }
-            else
-            {
-                HidePopup();
-            }
+            // isPopupVisible = !isPopupVisible;
+            popup.gameObject.SetActive(true);
+            StartCoroutine(HidePopupAfterDelay(popupDuration));
         }
 
-        private void ShowPopup()
-        {       
-            if (popup != null)
-            {
-                Vector3 popupPosition = transform.position + new Vector3(0, 0.5f, 0); // Adjust as needed
-                popup.transform.position = popupPosition;
-                popup.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-                isPopupVisible = true;
-            }
-            else
-            {
-                Debug.LogWarning("Popup does not exist.");
-            }    
-        }
-
-        private void HidePopup()
+        private IEnumerator HidePopupAfterDelay(float delay)
         {
-            if (popup != null)
-            {
-                popup.text = "";
-                isPopupVisible = false;
-            }
-            else
-            {
-                Debug.LogWarning("Popup does not exist.");
-            }
+            yield return new WaitForSeconds(delay);
+            popup.gameObject.SetActive(false);
         }
+
     }
 }
